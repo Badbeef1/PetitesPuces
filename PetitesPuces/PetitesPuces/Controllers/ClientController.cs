e7af27fa7909a4b08c855851ee4a8e6ee0b2e4ea
Nicolas Jouanique <N.Jouanique@cgodin.qc.ca>
2019-01-23 11:46:52 -0500

    Débuter la saisie de commande

---
+++ PetitesPuces/PetitesPuces/Controllers/ClientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetitesPuces.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult SaisieCommande()
+        {
+            return View();
+        }
+    }
+}
