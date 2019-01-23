2093cca971b6f3f99a6f34b9407a9330ccbe99b4
yhshan <yh.shan97@outlook.com>
2019-01-23 15:10:36 -0500

    Page inscription et oubli de mot de passe done et reliés ensemble

---
+++ PetitesPuces/PetitesPuces/Controllers/InscriptionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetitesPuces.Controllers
+{
+    public class InscriptionController : Controller
+    {
+        // GET: Inscription
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost]
+       public ActionResult VerifyEntry()
+       {
+
+          return null;
+       } 
+
+    }
+}
