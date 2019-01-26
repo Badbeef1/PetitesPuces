a367ab75c39d4e444ab6bbc84d0379a36179d20e
Yao Hua Shan <yh.shan97@outlook.com>
2019-01-25 21:45:01 -0500

    Login est fonctionnel i think

--- PetitesPuces/PetitesPuces/Controllers/ConnexionController.cs
+++ PetitesPuces/PetitesPuces/Controllers/ConnexionController.cs
@@ -1,40 +1,70 @@
 ﻿using System.Web.Mvc;
+using System.Linq;
 
 namespace PetitesPuces.Controllers
 {
-   public class ConnexionController : Controller
-   {
+    public class ConnexionController : Controller
+    {
 
-      // GET: Login
-      public ActionResult Index() => View();
+        // GET: Login
+        public ActionResult Index() => View();
 
-      [HttpPost]
-      public ActionResult VerifyLogin(Models.userExemple model)
-      {
-         var username = model.username;
-         var password = model.password;
+        [HttpPost]
+        public ActionResult VerifyLogin(Models.PPClientViewModel model)
+        {
+            var username = model.client.AdresseEmail?.ToLower();
+            var password = model.client.MotDePasse;
 
-         if (username == null || password == null)
-         {
-            model.errorMessage = "Vous avez oublié au moins un champ !";
-            model.password = (password != null) ? "" : null;  // null = red outline, "" = none
+            /* First checks  (Empty / IsAdmin) */
+            if (username == null || password == null)
+            {
+                model.errorMessage = "Vous avez oublié au moins un champ !";
+                model.client.MotDePasse = (password != null) ? "" : null;  // null = red outline, "" = none
+                return View("Index", model);
+            }
+            else if (username.ToLower().Equals("admin") && password == "Secret123")
+            {
+                Session["username"] = "Administrateur";
+                return RedirectToAction("Index", "Gestionnaire");
+            }
+
+
+            /* Compare data with Database */
+            Models.DataClasses1DataContext db = new Models.DataClasses1DataContext();
+            db.Connection.Open();
+            var clientLogged = (from cli in db.GetTable<Models.PPClients>()
+                                where cli.AdresseEmail.ToLower() == username &&
+                               cli.MotDePasse == password
+                                select cli).FirstOrDefault();
+
+            var vendeurLogged = (from ven in db.GetTable<Models.PPVendeurs>()
+                                 where ven.AdresseEmail.ToLower() == username.ToLower() &&
+                                ven.MotDePasse == password
+                                 select ven).FirstOrDefault();
+            db.Connection.Close();
+
+
+            if (clientLogged != null)
+            {
+                Session["username"] = clientLogged.AdresseEmail;
+                return RedirectToAction("Index", "Client");
+            }
+            else if (vendeurLogged != null)
+            {
+                Session["username"] = vendeurLogged.AdresseEmail;
+                Session["vendeurObj"] = vendeurLogged;
+                return RedirectToAction("Index", "Vendeur");
+            }
+            else
+            {
+                model.errorMessage = "Le courriel ou le mot de passe n'est pas valide.";
+                model.client.MotDePasse = ""; //No red border
+            }
+
             return View("Index", model);
-         }
-         else if (username.Equals("a") && password.Equals("a"))
-         {
-            Session["username"] = "Utilisateur test";
-         }
-         else
-         {
-            model.errorMessage = "Le courriel ou le mot de passe n'est pas valide.";
-            model.password = ""; //No red border
-            return View("Index", model);
-         }
+        }
 
-         return RedirectToAction("home_client", "Home");// Return to catalog
-      }
+        public ActionResult RecupererMDP() => View();
 
-      public ActionResult RecupererMDP() => View();
-      
-   }
+    }
 }
---
+++ PetitesPuces/PetitesPuces/Models/PPClientViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+    public class PPClientViewModel
+    {
+        public PPClients client { get; set; }
+        public PPVendeurs vendeur { get; set; }
+
+        public string errorMessage { get; set; }
+
+        [NotMapped] // Does not effect with your database
+        //[Compare("client.AdresseEmail")]
+        public string confirmUsername { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Password)]
+        //[Compare("client.MotDePasse")]
+        public string confirmPassword { get; set; }
+    }
+}
