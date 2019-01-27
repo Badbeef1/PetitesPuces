ce7f4db84b6285b8e1bfcb57be2c9bb2dff85e0f
Nicolas Jouanique <N.Jouanique@cgodin.qc.ca>
2019-01-26 20:26:37 -0500

    J'pense que je commence à comprendre

---
+++ PetitesPuces/PetitesPuces/Models/InactiviteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+    public class InactiviteViewModel
+    {
+        public List<PPClients> clients { get; set; }
+        public List<PPVendeurs> vendeurs { get; set; }
+
+    }
+
+
+
+}
