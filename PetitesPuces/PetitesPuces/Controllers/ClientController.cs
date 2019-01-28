1f3362f3ae12bb12c277e048902f87355080e377
bloc-notes <philippedoyon@outlook.com>
2019-01-27 22:13:40 -0500

    Gestion profils séparé en vue partiel, début affichage information profils et dossier pour les viewmodels

--- PetitesPuces/PetitesPuces/Controllers/ClientController.cs
+++ PetitesPuces/PetitesPuces/Controllers/ClientController.cs
@@ -8,6 +8,8 @@ namespace PetitesPuces.Controllers
 {
     public class ClientController : Controller
     {
+        Models.DataClasses1DataContext contextPP = new Models.DataClasses1DataContext();
+
         public ActionResult Index() => View("AccueilClient");
 
         public ActionResult AccueilClient() => View();
@@ -21,9 +23,51 @@ namespace PetitesPuces.Controllers
         // GET: ProduitDetail
         public ActionResult ProduitDetaille() => View();
 
-        public ActionResult GestionProfilClient() => View();
+        public ActionResult GestionProfilClient()
+        {
+            //HttpContext.User.Identity.Name
+            String strAdresseCourrielClient = "Client10000@cgodin.qc.ca";
+
+            var client = from unClient in contextPP.PPClients
+                         where unClient.AdresseEmail == strAdresseCourrielClient
+                         select unClient;
+
+            //client.ToList().ForEach(test => System.Diagnostics.Debug.WriteLine(test.AdresseEmail));
+            
+
+            return View(client.First());
+        }
         
         //Panier Détaillé du client
         public ActionResult PanierDetail() => View();
+
+        //Vue partiel Information personnel
+        [ChildActionOnly]
+        public ActionResult InformationPersonnel(PetitesPuces.Models.PPClients pp)
+        {
+            System.Diagnostics.Debug.WriteLine("Il passe ici la vue partiel!!!");
+
+            List<Models.Province> lstProvinces = new List<Models.Province>
+            {
+                new Models.Province { Abreviation = "AB", Nom = "Alberta"},
+                new Models.Province { Abreviation = "BC", Nom = "Colombie-Britanique"},
+                new Models.Province { Abreviation = "PE", Nom = "Île-du-Prince-Édouard"},
+                new Models.Province { Abreviation = "MB", Nom = "Manitoba"},
+                new Models.Province { Abreviation = "NB", Nom = "Nouveau-Brunswick"},
+                new Models.Province { Abreviation = "NS", Nom = "Nouvelle-Écosse"},
+                new Models.Province { Abreviation = "ON", Nom = "Ontario"},
+                new Models.Province { Abreviation = "QC", Nom = "Québec"},
+                new Models.Province { Abreviation = "SK", Nom = "Saskatchewan"},
+                new Models.Province { Abreviation = "NL", Nom = "Terre-Neuve-et-Labrador"}
+            };
+
+            ViewBag.ListeDesProvinces = new SelectList(lstProvinces, "Abreviation", "Nom");
+
+            return PartialView();
+        }
+
+        //Vue partiel modification du mot de passe
+        [ChildActionOnly]
+        public ActionResult ModificationMDP() => PartialView();
    }
 }
---
+++ PetitesPuces/PetitesPuces/Models/Province.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+    public class Province
+    {
+        public String Abreviation { get; set; }
+        public String Nom { get; set; }
+    }
+}
