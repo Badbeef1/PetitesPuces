25e8dfa86c538f264a1cae630901474d0f984b5a
Nicolas Jouanique <N.Jouanique@cgodin.qc.ca>
2019-01-28 23:45:15 -0500

    Wowwoowowow le déboucher de la mort Checkpoint

--- PetitesPuces/PetitesPuces/Controllers/GestionnaireController.cs
+++ PetitesPuces/PetitesPuces/Controllers/GestionnaireController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using PetitesPuces.Models;
 
 namespace PetitesPuces.Controllers
@@ -17,23 +18,31 @@ namespace PetitesPuces.Controllers
 
         public ActionResult GestionInactivite()
         {
-            IList<SelectListItem> lstClient = new List<SelectListItem>();
-            IList<SelectListItem> lstVendeur = new List<SelectListItem>();
-
-            for (int i = 0; i < dc.GetTable<PPClients>().ToList().Count; i++)
+            List<Inactiver> lstClient = new List<Inactiver>();
+            List<Inactiver> lstVendeur = new List<Inactiver>();
+            int counter = 0;
+            foreach (PPClients client in dc.GetTable<PPClients>().ToList())
             {
-                lstClient.Add(new SelectListItem
-                {
-                    Value = dc.GetTable<PPClients>().ToList()[i].NoClient.ToString()
-                });
+                lstClient.Add(
+                    new Inactiver
+                    {
+                        ID =  counter,
+                        IsSelected = false,
+                        idClient = client.NoClient.ToString()
+                    });
+                counter++;
             }
-
-            for (int i = 0; i < dc.GetTable<PPVendeurs>().ToList().Count; i++)
+            counter = 0;
+            foreach (PPVendeurs vendeur in dc.GetTable<PPVendeurs>().ToList())
             {
-                lstVendeur.Add(new SelectListItem
-                {
-                    Value = dc.GetTable<PPVendeurs>().ToList()[i].NoVendeur.ToString()
-                });
+                lstVendeur.Add(
+                    new Inactiver
+                    {
+                        ID = counter,
+                        IsSelected = false,
+                        idClient = vendeur.NoVendeur.ToString()
+                    });
+                counter++;
             }
 
             InactiviteViewModel iVM = new InactiviteViewModel
@@ -48,9 +57,66 @@ namespace PetitesPuces.Controllers
         }
 
         [HttpPost]
-        public ActionResult GestionInactivite(List<SelectListItem> items)
+        public ActionResult GestionInactivite(InactiviteViewModel form)
         {
-            return View();
+            List<PPClients> lstClientInfo = new List<PPClients>();
+            List<PPClients> lstClientDelete = new List<PPClients>();
+            List<PPVendeurs> lstVendeurInfo = new List<PPVendeurs>();
+            List<Inactiver> lstClients = new List<Inactiver>();
+            List<Inactiver> lstVendeurs = new List<Inactiver>();
+            PPClients clientRetirer = new PPClients();
+            //Retirer client
+            foreach (Inactiver client in form.cbClients)
+            {
+                if(client.IsSelected == true)
+                {
+                    clientRetirer = null;
+                    if (dc.GetTable<PPVendeursClients>().Where(m => m.NoClient.ToString() == client.idClient).ToList().Count <= 0)
+                    {
+                        foreach(PPClients clientBDD in dc.GetTable<PPClients>().ToList())
+                        {
+                            if(clientBDD.NoClient.ToString() == client.idClient)
+                            {
+                                lstClientDelete.Add(clientBDD);
+                            }
+                        }
+
+                    }
+                      
+                }
+                else
+                {
+                    lstClients.Add(client);
+                }
+            }
+            dc.Connection.Open();
+            dc.GetTable<PPClients>().DeleteAllOnSubmit(lstClientDelete);
+            dc.SubmitChanges();
+            dc.Connection.Close();
+            //Retirer vendeur
+            foreach (Inactiver vendeur in form.cbVendeurs)
+            {
+                if (vendeur.IsSelected == false)
+                {
+                    foreach (PPVendeurs vendeurInfos in dc.GetTable<PPVendeurs>().ToList())
+                    {
+                        if (vendeurInfos.NoVendeur.ToString() == vendeur.idClient)
+                        {
+                            lstVendeurInfo.Add(vendeurInfos);
+                        }
+                    }
+                    lstVendeurs.Add(vendeur);
+                }
+            }
+            InactiviteViewModel renvoyer = new InactiviteViewModel
+            {
+                clients = dc.GetTable<PPClients>().ToList(),
+                vendeurs = lstVendeurInfo,
+                cbClients = lstClients,
+                cbVendeurs = lstVendeurs
+            };
+            ModelState.Clear();
+            return View("GestionInactivite", renvoyer);
         }
 
         public ActionResult Statistiques() => View();
