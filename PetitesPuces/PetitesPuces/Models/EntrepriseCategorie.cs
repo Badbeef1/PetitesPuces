82dbb1c572f83b743b23d2bf59ea4f6c3fc555bc
Badbeef1 <malboeufsimon@gmail.com>
2019-01-30 17:45:24 -0500

    regler les problèmes de la page d'accueil internaute

--- PetitesPuces/PetitesPuces/Models/EntrepriseCategorie.cs
+++ PetitesPuces/PetitesPuces/Models/EntrepriseCategorie.cs
@@ -5,15 +5,15 @@ using System.Web;
 
 namespace PetitesPuces.Models
 {
-    public class EntrepriseCategorie
-    {
-        public Models.PPCategories categorie { get; set; }
-        public List<Models.PPVendeurs> lstVendeurs { get; set;}
+   public class EntrepriseCategorie
+   {
+      public Models.PPCategories categorie { get; set; }
+      public List<Models.PPVendeurs> lstVendeurs { get; set; }
 
-        public EntrepriseCategorie(PPCategories categorie, List<PPVendeurs> lstVendeurs)
-        {
-            this.categorie = categorie;
-            this.lstVendeurs = lstVendeurs;
-        }
-    }
+      public EntrepriseCategorie(PPCategories categorie, List<PPVendeurs> lstVendeurs)
+      {
+         this.categorie = categorie;
+         this.lstVendeurs = lstVendeurs;
+      }
+   }
 }
