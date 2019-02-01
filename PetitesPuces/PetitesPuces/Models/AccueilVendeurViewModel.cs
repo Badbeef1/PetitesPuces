e06457e36e793e3e09b403fc88d2bb083b9d3ce2
Simon Malboeuf <simonmalboeuf@outlook.com>
2019-02-01 11:58:29 -0500

    debut de la page accueil client avec informations de la BD

---
+++ PetitesPuces/PetitesPuces/Models/AccueilVendeurViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.Models
+{
+   public class AccueilVendeurViewModel
+   {
+      public Dictionary<PPCommandes, List<PPDetailsCommandes>> lstCommandesNonTraites { get; set; }
+      public IQueryable<IGrouping<PPClients,PPArticlesEnPanier>> lstClientPanier { get; set; }
+      public int nbVisites { get; set; }
+
+      public AccueilVendeurViewModel(Dictionary<PPCommandes, List<PPDetailsCommandes>> lstCommandesNonTraites, IQueryable<IGrouping<PPClients, PPArticlesEnPanier>> lstClientPanier, int nbVisites)
+      {
+         this.lstCommandesNonTraites = lstCommandesNonTraites;
+         this.lstClientPanier = lstClientPanier;
+         this.nbVisites = nbVisites;
+      }
+   }
+}
