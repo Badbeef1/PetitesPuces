818bc7a8a4a5aee116f02aa36fdf9ecc802ee0dc
Bloc-notes <philippedoyon@outlook.com>
2019-01-31 19:35:55 -0500

    Début programmation page catalogue

---
+++ PetitesPuces/PetitesPuces/ViewModels/CatalogueViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetitesPuces.ViewModels
+{
+    public class CatalogueViewModel
+    {
+        public List<Models.PPCategories> lstCategorie { get; set; }
+        public List<Models.PPProduits> lstproduits { get; set; }
+    }
+}
