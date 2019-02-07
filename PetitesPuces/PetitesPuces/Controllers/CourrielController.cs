6cf607d9d34e6a168102f8936ebc397ece0f5647
Bloc-notes <philippedoyon@outlook.com>
2019-02-06 20:48:58 -0500

    Commencement de l'interface du module courriel et style pour fixer les bar de navigation vertical

---
+++ PetitesPuces/PetitesPuces/Controllers/CourrielController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetitesPuces.Views
+{
+    public class CourrielController : Controller
+    {
+        // GET: Courriel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
